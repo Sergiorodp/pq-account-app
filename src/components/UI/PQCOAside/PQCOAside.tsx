2aaa6dee50fe4ad37978bc975b5b4f70348a5fbb
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-16 21:57:12 -0500

    feat(PQCOAside): create new component aside

---
+++ src/components/UI/PQCOAside/PQCOAside.tsx
@@ -0,0 +1,10 @@
+import styles from "./PQCOAside-styles.module.scss";
+
+export default function PQCOAside() {
+  return (
+    <nav className={styles.aside_container}>
+        <h1>gdgsdfds</h1>
+    </nav>
+    
+  );
+}
