ede0e71123e85a10d77fc9fb283ede3d44f7ee3d
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-17 07:44:31 -0500

    feat(cds): create custom components

---
+++ src/components/UI/PQCOCDS/PQCOGrid.tsx
@@ -0,0 +1,8 @@
+
+export default function PQCOGridCds({children} : Readonly<{children: React.ReactNode}>){
+    return(
+        <div className="cds--css-grid">
+            {children}
+        </div>
+    )
+}
