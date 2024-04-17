f850b846f3f237bd0341b4900eeaf9129fe7c542
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-16 21:57:30 -0500

    feat(NAV): change nav styles

--- src/components/UI/PQCONav/PQCONavUi.tsx
+++ src/components/UI/PQCONav/PQCONavUi.tsx
@@ -5,18 +5,22 @@ export default function PQCONav(){
     const links = ['hello', 'chao']
 
     return(
-        <nav className={styles.nav_container}>
-            <ul className={styles.nav_list}>
-                {
-                    links.map( link => {
-                        return(
-                            <li key={link}>
-                                <span>{link}</span>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+        <nav className={ `${styles.nav_container}`}>
+            <div className="cds--css-grid">
+            <div className='cds--css-grid-column cds--sm:col-span-4 cds--md:col-span-8 pq--lg:col-span-16'>
+                <ul className={`${styles.nav_list}`}>
+                    {
+                        links.map( link => {
+                            return(
+                                <li key={link}>
+                                    <span>{link}</span>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+            </div>
         </nav>
     )
 }
