458a135e24f2f9e765fd27096623d79001df4058
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-24 20:50:30 -0500

    feat(auth): added auth pages

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,14 @@
+import PQCOGridCds from '@app/components/UI/PQCOCDS/PQCOGrid'
+import PQCOColumn from '@app/components/UI/PQCOCDS/PQCOColumn'
+
+export default function SignIn(){
+    return(
+        <PQCOGridCds>
+            <PQCOColumn lg='4' lg_start='7'>
+                <div>
+                    Sign In
+                </div>
+            </PQCOColumn>
+        </PQCOGridCds>
+    )
+}
