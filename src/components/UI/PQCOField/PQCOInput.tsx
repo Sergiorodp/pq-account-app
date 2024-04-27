4633bad42483069fd16192d8bbb3273abe93d298
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-26 23:19:47 -0500

    feat(formField): create input skeleton

---
+++ src/components/UI/PQCOField/PQCOInput.tsx
@@ -0,0 +1,17 @@
+//TODO create form field interface
+export default function Input({
+    labelName,
+    type,
+    name,
+    placeholder,
+    value,
+    handleChange,
+    isSurpriseMe,
+    handleSurpriseMe
+}) {
+    return(
+        <input type={type}>
+
+        </input>
+    )
+}
