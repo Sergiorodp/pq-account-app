e97e716305fc8d76d29f966ec775f3036d580dcd
Sergio_Rodriguez_Perez <sergio.rod.perez@outlook.es>
2024-04-29 23:20:28 -0500

    feat(src/app/page): added route to login when user is not in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import APP_CONFIG from "@app/app/config/routes";
 
 export default function Home() {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if(true){
+      router.push(APP_CONFIG.APP_ROUTES.login)
+    }
+  },[])
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        Hola mundo
+        <p>Hola mundo </p>
       </div>
     </main>
   );
